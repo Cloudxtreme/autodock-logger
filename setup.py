b952b13925de05d351d456e0f4453a9f7e08ecbd
James Mills <prologic@shortcircuit.net.au>
2015-03-23 00:39:28 +1000

    Updated depdencies

--- setup.py
+++ setup.py
@@ -9,10 +9,5 @@ setup(
     version="0.0.1",
     description="autodock logger plugin",
     install_requires=[],
-    # XXX: Use upstream dependencies
-    # install_requires=[
-    #     "autodock",
-    #     "circuits"
-    # ],
     scripts=["autodock-logger"]
 )
