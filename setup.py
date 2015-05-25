9e942a28e9a6d6587b93d5695647b1f61cfe91ea
James Mills <prologic@shortcircuit.net.au>
2015-05-26 09:39:04 +1000

    Fixed packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python
 
+from setuptools import setup, find_packages
 
-from setuptools import setup
+
+def parse_requirements(filename):
+    with open(filename, "r") as f:
+        for line in f:
+            if line and line[0] != "#":
+                yield line.strip()
 
 
 setup(
     name="autodock-logger",
     version="0.0.1",
     description="autodock logger plugin",
-    install_requires=[],
+    long_description=open("README.rst", "r").read(),
+    author="James Mills",
+    author_email="James Mills, prologic at shortcircuit dot net dot au",
+    url="https://github.com/prologic/autodock-logger",
+    download_url="https://github.com/prologic/autodock-logger/archive/master.zip",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    license="MIT",
+    keywords="autodock logger plugin docker",
+    platforms="POSIX",
+    packages=find_packages("."),
+    install_requires=list(parse_requirements("requirements.txt")),
     scripts=["autodock-logger"]
 )
